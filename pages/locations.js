24c84e90446ad85b6dfec963792303fcc26d72d6
Joseph Moran <josephdeargmoran@protonmail.com>
2022-11-03 14:21:35 -0400

    More spacing fixes, will probably need to be fixed in a "better" way down the line

--- pages/locations.js
+++ pages/locations.js
@@ -16,6 +16,8 @@ function makeLocationCard(location){
             </span>
             <span class="locationCardRight">
                 ${location.name}
+                <br>
+                ${location.location}
             </span>
         </span>
     `
@@ -33,6 +35,16 @@ function main(){
     locations.push(new DiningLocation("Beanz", "Grace Watson Hall"))
     locations.push(new DiningLocation("Sol Underground", "Tunnel"))
     locations.push(new DiningLocation("Artesano's", "Student Alumni Union"))
+    
+    //Temp adding extra to make sure it works as expected 
+    locations.push(new DiningLocation("Ctrl Alt DELi", "Golisano Hall"))
+    locations.push(new DiningLocation("The Commons", "N/A"))
+    locations.push(new DiningLocation("Gracies", "Grace Watson Hall"))
+    locations.push(new DiningLocation("Crossroads", "Global Village"))
+    locations.push(new DiningLocation("Brick City", "Student Alumni Union"))
+    locations.push(new DiningLocation("Beanz", "Grace Watson Hall"))
+    locations.push(new DiningLocation("Sol Underground", "Tunnel"))
+    locations.push(new DiningLocation("Artesano's", "Student Alumni Union"))
 
 
     console.log(document)
@@ -42,6 +54,11 @@ function main(){
     locations.forEach(location => {
         locationsBox.innerHTML += makeLocationCard(location)
     });
+
+    //Add lower break because I need it temporarily
+    for(let i = 0; i < 5; i++){
+        locationsBox.innerHTML += "<br>"
+    }
 
 }
 
