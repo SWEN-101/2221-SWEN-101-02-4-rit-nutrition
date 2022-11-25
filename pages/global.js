57633d777598a763b36cad6f9ed8a5c1cdea5812
Joseph Moran <josephdeargmoran@protonmail.com>
2022-11-25 15:05:23 -0500

    Passing location name through pages

--- pages/global.js
+++ pages/global.js
@@ -1,6 +1,37 @@
+class DiningLocation {
+    constructor(name, location, schedule = null){ //Schedule will eventually be it's own object, but for the time being just gonna leave it as null
+        this.name = name
+        this.location = location
+    }
+}
+
+//Going to store the locations out of my objects and load whenever this is loaded
+locations = []
+
+//Locations Data
+locations.push(new DiningLocation("Ctrl Alt DELi", "Golisano Hall"))
+locations.push(new DiningLocation("The Commons", "N/A"))
+locations.push(new DiningLocation("Gracies", "Grace Watson Hall"))
+locations.push(new DiningLocation("Crossroads", "Global Village"))
+locations.push(new DiningLocation("Brick City", "Student Alumni Union"))
+locations.push(new DiningLocation("Beanz", "Grace Watson Hall"))
+locations.push(new DiningLocation("Sol Underground", "Tunnel"))
+locations.push(new DiningLocation("Artesano's", "Student Alumni Union"))
+locations.push(new DiningLocation("Ctrl Alt DELi", "Golisano Hall"))
+locations.push(new DiningLocation("The Commons", "N/A"))
+locations.push(new DiningLocation("Gracies", "Grace Watson Hall"))
+locations.push(new DiningLocation("Crossroads", "Global Village"))
+locations.push(new DiningLocation("Brick City", "Student Alumni Union"))
+locations.push(new DiningLocation("Beanz", "Grace Watson Hall"))
+locations.push(new DiningLocation("Sol Underground", "Tunnel"))
+locations.push(new DiningLocation("Artesano's", "Student Alumni Union"))
+
+localStorage.setItem("locationData", JSON.stringify(locations))
+
 class PersistantData { //This object will be used to store all of our persistent data, this will be used on the location specific page
     selectedLocation = null
     selectedCategory = null
+    locations = null
     save(){
         localStorage.setItem("storedData", JSON.stringify(this))
     }
@@ -11,10 +42,13 @@ class PersistantData { //This object will be used to store all of our persistent
         }else{
             this.selectedLocation = loadedObject.selectedLocation
             this.selectedCategory = loadedObject.selectedCategory
-        }
-        
+            this.locations = JSON.parse(localStorage.getItem("locationData"))
+        }    
     }
-   
+    clear(){
+        this.selectedLocation = null
+        this.selectedCategory = null
+    }
 }
 
 storedData = new PersistantData()
--- pages/locations.js
+++ pages/locations.js
@@ -1,15 +1,6 @@
-// For the time being I am going to store the dining locations inside of objects to be referenced later and to pull information from
-
-class DiningLocation {
-    constructor(name, location, schedule = null){ //Schedule will eventually be it's own object, but for the time being just gonna leave it as null
-        this.name = name
-        this.location = location
-    }
-}
-
 function makeLocationCard(location){
     return `
-        <span class = "locationCard">
+        <span class = "locationCard" onclick="selectLocation('${location.name}')">
             <span class="locationCardLeft">
                 <img src="assets/ctrlalt.jpg"/>
                 Left
@@ -24,34 +15,19 @@ function makeLocationCard(location){
     
 }
 
+function selectLocation(location){
+    storedData.selectedLocation = location
+    storedData.save()
+    window.location.href = "location-details.html"
+}
+
+storedData = new PersistantData()
+storedData.load()
+
+
 function main(){
-    locations = []
-
-    locations.push(new DiningLocation("Ctrl Alt DELi", "Golisano Hall"))
-    locations.push(new DiningLocation("The Commons", "N/A"))
-    locations.push(new DiningLocation("Gracies", "Grace Watson Hall"))
-    locations.push(new DiningLocation("Crossroads", "Global Village"))
-    locations.push(new DiningLocation("Brick City", "Student Alumni Union"))
-    locations.push(new DiningLocation("Beanz", "Grace Watson Hall"))
-    locations.push(new DiningLocation("Sol Underground", "Tunnel"))
-    locations.push(new DiningLocation("Artesano's", "Student Alumni Union"))
-    
-    //Temp adding extra to make sure it works as expected 
-    locations.push(new DiningLocation("Ctrl Alt DELi", "Golisano Hall"))
-    locations.push(new DiningLocation("The Commons", "N/A"))
-    locations.push(new DiningLocation("Gracies", "Grace Watson Hall"))
-    locations.push(new DiningLocation("Crossroads", "Global Village"))
-    locations.push(new DiningLocation("Brick City", "Student Alumni Union"))
-    locations.push(new DiningLocation("Beanz", "Grace Watson Hall"))
-    locations.push(new DiningLocation("Sol Underground", "Tunnel"))
-    locations.push(new DiningLocation("Artesano's", "Student Alumni Union"))
-
-
-    console.log(document)
     locationsBox = document.getElementById("mainBodyContent");
-    console.log(locationsBox)
-    
-    locations.forEach(location => {
+    storedData.locations.forEach(location => {
         locationsBox.innerHTML += makeLocationCard(location)
     });
 
