0c39fc12b93a4abb97f84249b0b4e72024d68db1
Joseph Moran <josephdeargmoran@protonmail.com>
2022-11-27 23:47:57 -0500

    Food class as prep for loading in foods for the pages

--- pages/global.js
+++ pages/global.js
@@ -1,15 +1,31 @@
+class FoodItem {
+    constructor(name, price, calories, image_path){
+        this.name = name
+        this.price = price
+        this.calories = calories
+        this.image = image_path
+    }
+}
+
+//Going to store all of the food objects in this list
+let foods = []
+
+//This is where all the food data will be pushed
+foods.push(null)
+
 class DiningLocation {
     constructor(name, location, image_path){ //Schedule will eventually be it's own object, but for the time being just gonna leave it as null
         this.name = name
         this.location = location
         this.image = image_path
+        this.food = [] //Every location is going to have the same selection of food because time is tight and SWEN 101 wants a good looking project not a working project which is silly but yeah
     }
 }
 
 //Going to store the locations out of my objects and load whenever this is loaded
-locations = []
+let locations = []
 
-//Locations Data
+//Locations Data, have to push like this because I was getting weird crashing issues when I assigned them within the list
 locations.push(new DiningLocation("Ctrl Alt DELi", "Golisano Hall", "assets/locations/ctrlalt.jpg"))
 locations.push(new DiningLocation("The Commons", "Briggs", "assets/locations/commons.jpeg"))
 locations.push(new DiningLocation("Gracies", "Grace Watson Hall", "assets/locations/gracies.jpeg"))
